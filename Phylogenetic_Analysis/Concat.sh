24c40052457b6c4bbf08dc35d515eaa3aa11cdb8
Frances Pitsillides <70023430+ffrapi@users.noreply.github.com>
2022-07-08 16:57:18 +0100

    Create Concat.sh

---
+++ Phylogenetic_Analysis/Concat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH -p all
+#SBATCH -J Concat_Neoc_job
+#SBATCH -t 0-3:00:00
+#SBATCH -o /data/projects/gaya_lab/Frances/Neocucurbitaria
+
+#Concatenate gene alignments
+
+conda activate AMAS
+
+AMAS.py concat -f fasta -d dna -i marker_seqs_ITS_alntrimmed.fa marker_seqs_LSU_alntrimmed.fa marker_seqs_RBP2_alntrimmed.fa marker_seqs_SSU_alntrimmed.fa marker_seqs_Tef1_alntrimmed.fa marker_seqs_Tub2_alntrimmed.fa  -p Neocucurbitaria_partition.txt -t Neocucurbitaria_concat.phy -u phylip
+
+#Add gene models
+sed -i 's/^/GTR+G, /' Neocucurbitaria_partition.txt
