c36a799ec758822561bbe5b9be0e34ab86dbb714
Frances Pitsillides <70023430+ffrapi@users.noreply.github.com>
2022-07-25 15:19:21 +0100

    Update raxml.sh

--- Phylogenetic_Analysis/raxml.sh
+++ Phylogenetic_Analysis/raxml.sh
@@ -3,32 +3,34 @@
 #SBATCH -p all
 #SBATCH -J raxml_Neoc_job
 #SBATCH -t 0-3:00:00
-#SBATCH -o /data/projects/gaya_lab/Frances/Neocucurbitaria/raxml
+#SBATCH -o /data/projects/gaya_lab/Frances/Neocucurbitaria/endophyte_genes/align/Jul25/raxml.its.out
+#SBATCH -e /data/projects/gaya_lab/Frances/Neocucurbitaria/endophyte_genes/align/Jul25/raxml.its.err
 
 
-module load anaconda3
-conda activate raxml-ng
+
+module load anaconda
+module load raxml-ng
 
 #Convert file for RAxML-NG
 
 raxml-ng --parse \
-         --msa Neocucurbitaria_concat.phy \
-         --model Neocucurbitaria_partition.txt \
-         --prefix Neoc
+         --msa Neocucurbitaria_concat_its.phy \
+         --model Neocucurbitaria_partition_its.txt \
+         --prefix Neoc.its
 
 #Run ML tree search and bootstrapping for 1000 iterations
 
 raxml-ng --all \
-         --msa Neocucurbitaria_concat.phy \
-         --model Neocucurbitaria_partition.txt \
-         --prefix Neoc \
+         --msa Neocucurbitaria_concat_its.phy \
+         --model Neocucurbitaria_partition_its.txt \
+         --prefix Neoc.its \
          --seed 2 \
-         --threads ${NSLOTS} \
+         --threads auto \
          --bs-trees 1000
 
 #Check convergence
 
 raxml-ng --bsconverge \
-         --bs-trees 1000 \
-         --prefix Neoc_convergence_test \
+         --bs-trees Neoc.its.raxml.bootstraps \
+         --prefix Neoc_its_convergence_test \
          --seed 2
