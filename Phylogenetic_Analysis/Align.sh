c6a6ecbcf8b138707a71b3f2797b2652fee1c678
Frances Pitsillides <70023430+ffrapi@users.noreply.github.com>
2022-07-25 14:43:52 +0100

    Update Align.sh

--- Phylogenetic_Analysis/Align.sh
+++ Phylogenetic_Analysis/Align.sh
@@ -3,38 +3,17 @@
 #SBATCH -p all
 #SBATCH -J align_Neoc_job
 #SBATCH -t 0-3:00:00
-#SBATCH -o /data/projects/gaya_lab/Frances/Neocucurbitaria/align.out
-#SBATCH -e /data/projects/gaya_lab/Frances/Neocucurbitaria/align.err
+#SBATCH -o /data/projects/gaya_lab/Frances/Neocucurbitaria/endophyte_genes/align/Jul25/align.out
+#SBATCH -e /data/projects/gaya_lab/Frances/Neocucurbitaria/endophyte_genes/align/Jul25/align.err
 
 
 #Create alignments for each gene
 
 module load mafft
 
-mafft marker_seqs_ITS.fa > marker_seqs_ITS_aln.fa
-mafft marker_seqs_LSU.fa > marker_seqs_LSU_aln.fa
-mafft marker_seqs_RPB2.fa > marker_seqs_RPB2_aln.fa
-mafft marker_seqs_SSU.fa > marker_seqs_SSU_aln.fa
-mafft marker_seqs_Tef1.fa > marker_seqs_Tef1_aln.fa
-mafft marker_seqs_Tub2.fa > marker_seqs_Tub2_aln.fa
-
-
-#!/bin/bash
-#SBATCH -c 1
-#SBATCH -p all
-#SBATCH -J align_Neoc_job
-#SBATCH -t 0-3:00:00
-#SBATCH -o /data/projects/gaya_lab/Frances/Neocucurbitaria/endophyte_genes/align/align.out
-#SBATCH -e /data/projects/gaya_lab/Frances/Neocucurbitaria/endophyte/genes/align/align.err
-
-
-#Create alignments for each gene
-
-module load mafft
-
-mafft marker_seqs_ITS_FINAl.fa > marker_seqs_ITS_aln.fa
-mafft marker_seqs_LSU_FINAL.fa > marker_seqs_LSU_aln.fa
-mafft marker_seqs_RPB2_FINAL.fa > marker_seqs_RPB2_aln.fa
-mafft marker_seqs_SSU_FINAL.fa > marker_seqs_SSU_aln.fa
-mafft marker_seqs_Tef1_FINAL.fa > marker_seqs_Tef1_aln.fa
-mafft marker_seqs_Tub2_FINAL.fa > marker_seqs_Tub2_aln.fa
+mafft ITS_aln_rename_FP1.fa > marker_seqs_ITS_aln.fa
+mafft SSU_aln_rename_FP1.fa > marker_seqs_LSU_aln.fa
+mafft LSU_aln_rename_FP1.fa > marker_seqs_RPB2_aln.fa
+mafft RPB2_aln_rename_FP1.fa > marker_seqs_SSU_aln.fa
+mafft TEF1_aln_rename_FP1.fa > marker_seqs_Tef1_aln.fa
+mafft TUB2_aln_rename_FP1.fa > marker_seqs_Tub2_aln.fa
